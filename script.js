33257f0d5be254f0e9191a0e8519e2656afad28b
Jerad Bitner <sirkitree@gmail.com>
2023-12-19 12:41:50 -0800

    updating employees list

--- script.js
+++ script.js
@@ -1,9 +1,207 @@
-let employees_0 = ["Adams, Alexander", "Addy, Cabral Ayitey", "Aderhold, Julie", "Alsina, Marc Anthony", "Anderson, Daniel", "Anderson, Daniel A", "Andrews, Ryan Nikolas", "Andricak, Brittany", "Andujar Batista, Reinaldo", "Arnold, Ricky E", "Bailey, Charles K", "Baker, Cory Hawk", "Baskin, Roger", "Bastian, Brent", "Bean, Alan James", "Beattie, Jeremiah Donald", "Beck, Adam", "Beck, Crystal Ann", "Beck, Matthew", "Bennage, Brandon", "Bennage, Kimberly J", "Bennage, Ricky T", "Benscoter, Bradley S", "Bergerstock, Cheyenne", "Bergerstock, Todd D", "Blake, Aysha", "Bower, Damian Allen", "Bowersox, Jacob Michael", "Byerly, Jeffrey", "Campbell, Richard", "Catherman, Michael A", "Clark, Timothy Aaron", "Clayton, Tyler Scott", "Cook, Richard K", "Cook, Tyler Eugene", "Cotner, David", "Coup, Graydon", "Day, Eric", "Deitrich, Mariah", "Delsite, Douglas", "Doebler, Douglas Lee", "Donfrancesco, Nicholas C", "Dries, Amanda", "Drummond, Nastassja", "Ebling, Steven Craig", "Elliott, Jayson", "Eltringham, Kevin Eric", "Farley, James L", "Feigles, Pamela N", "Ferguson, Michael Lee"];
-let employees_1 = ["Figueroa, Jose Anthonio", "Figueroa, Osvaldo", "Fitzwater, Todd", "Fondeur, Ricardo Hugo", "Forker, Nathan Daniel", "Foster, Ian Allen", "France, Steven", "Frederick, Adam", "Fry, Wayne A", "Funk, Joshua", "Gajus, Wesley Brandon", "Gardiner, Angela J", "Gardiner, Cadence Leiter", "Garrison, Courtney", "Gearhart III, Carl P", "Gehrer, Alexander", "Gehrer, Austin", "Gessner, Marcus", "Glica, Brendan", "Gomez, Osvaldo", "Gross, Caleb Jeffrey-Jo", "Guffey, Brian", "Haas, Bobbi Jo", "Hall, Thomas W", "Harner, Justin Sterling", "Helsley III, Stephen", "Helsley, Stephen", "Heverly, Brian K", "Hoffman, William D", "Huhn, Christopher", "Hulsizer, Brian W", "Ikeler, William", "Irwin, Tammie S", "Jean, Shana", "Jeremiah, Tyler", "Johnson, Kelvin James", "Jones, Malcom", "Kane, Dylan Robert", "Kearney, Brian J", "Kiesinger, Sonya L", "Kiger, Steven L", "Kirby, Eric Matthew", "Kline, Justin Allen", "Klinger, Robert L.", "Knorr, Angela K", "Koch, Carson D", "Kratzer, Kean", "Kremer, Jodie", "Kurtz, Evan", "Laforme, Jordan"]
-let employees_2 = ["Leon, Bradley Scott", "Leon, Brent Lee", "Leon, Devon Charles", "Lindauer, Katelyn", "Livziey, Blaze Quinton", "Long, Allana Faye", "Long, Gregory Jonathan", "Long, Jonathon Ray", "Loss, John D", "Lucas, Christopher P", "Lutz, Brian", "Mabus, Kristen", "Martin, Chad", "Martz, Twylia-Dawn", "Maxfield, Timothy L", "McCarrick, Jason", "Miller, Keith A", "Miller, Robert D", "Miller, Tonya M", "Mingle, Charles Forrest", "Minium, Greg", "Montague, Nicholas Ryan", "Morse, Matthew", "Moyer, Curtis", "Noll, Bonnie M", "Noll, Dennis M", "Oeler, Heather M", "Ortiz, Luis Anibal", "Parker, Dustin", "Parker, Garrett", "Parrish, Shannon", "Persun, Jeremy L", "Peterson, Donovan Scott", "Porras, Jairo", "Price, Davon Marchone", "Prioleau, James Theodore", "Pulicicchio, Daren", "Ranck, McKenzie", "Reber, Brittany Ann", "Reedy, Drew T", "Reedy, Millard E", "Reigle, Kevin Eugene", "Rewolinski, Ty S", "Reynolds, Michele R", "Robbins, Colby William", "Robbins, Lonny D", "Roberts, Evelyn", "Rosado Colon, Luis A", "Ross, Chad", "Ross, Sabrena Anita-Mae"]
-let employees_3 = ["Rovenolt, Cathy Ann", "Sampsell, Brian Eugene", "Schreck, Kevin Dean", "Seese, Dio M", "Shipton, Blake", "Sholter, Rodney H", "Short, Charles S", "Silverman, Molly", "Simmon, Scott E", "Simon, Brandon A", "Simon, Laura M", "Sims, Brian", "Smith, Cody Cheyenne", "Smith, Cody Christopher", "Smith, Kurtis Alonzo", "Snyder, Joshua James", "Snyder, Justin", "Snyder, Scott Douglas", "Staplins, Brittany Marie", "Stokes, Donnell", "Stoltzfus, Patricia C", "Strahan, David E", "Straub, Marshall Lamar", "Strohl, Jason Wade", "Susan, Eric S", "Taggart, Ginger M", "Tallman, Zachary A", "Topliff, Nicolas D", "Treibley, Kenneth M", "Try, Jeffrey", "Ulrich Jr., Timothy Leroy", "Ulrich, Robert M", "Vanover, Ronda L", "Wagner, Aaron Ray", "Wagner, Dylan Wesley", "Wagner, Walter", "Walters, Chad", "Weaver, Eric E", "Wells Biniskandar, Muhammad Geoffrey", "Wertz, Eric Leroy", "Wesner, Preston", "West, Michael D", "Whalen Jr., Frank James", "White, Annette Yvonne", "Willis, Brent Ryan", "Wilson, Kyle Anthony", "Wise, Dashawn", "Wood, Joseph Lynn", "Wright, Carolyn", "Young, Thomas"]
-let employees_4 = ["Zettlemoyer, Ronald E", "Clayton, Scott E", "Bayerl, Polly A", "DeWald II, William John", "Peters, Francis B", "Schmidt, Joseph George", "Stroup, Jon Christopher", "Weiser, Nathan", "Raup, Bonnie S", "Troxell, Marlin W", "Bingaman, Joshua", "Cooper, Chad M", "Fletcher, Theodore C", "Hall, Edward Allen", "Henrie, Zachary", "Kehres, Terry", "Vincent, Troy A", "Wise, Justin", "Lasch, Todd"]
-let employees = employees_0.concat(employees_1, employees_2, employees_3, employees_4);
+let employees = `Adams, Alexander
+Addy, Cabral Ayitey
+Aderhold, Julie
+Alsina, Marc Anthony
+Anderson, Daniel
+Anderson, Daniel A
+Andrews, Ryan Nikolas
+Andricak, Brittany
+Andujar Batista, Reinaldo
+Arnold, Ricky E
+Bailey, Charles K
+Baker, Cory Hawk
+Baskin, Roger
+Bastian, Brent
+Bean, Alan James
+Beattie, Jeremiah Donald
+Beck, Adam
+Beck, Crystal Ann
+Beck, Matthew
+Bennage, Brandon
+Bennage, Kimberly J
+Bennage, Ricky T
+Benscoter, Bradley S
+Bergerstock, Cheyenne
+Bergerstock, Todd D
+Blake, Aysha
+Bower, Damian Allen
+Bowersox, Jacob Michael
+Byerly, Jeffrey
+Campbell, Richard
+Catherman, Michael A
+Clark, Timothy Aaron
+Clayton, Tyler Scott
+Cook, Richard K
+Cook, Tyler Eugene
+Cotner, David
+Coup, Graydon
+Day, Eric
+Deitrich, Mariah
+Delsite, Douglas
+Doebler, Douglas Lee
+Donfrancesco, Nicholas C
+Dries, Amanda
+Drummond, Nastassja
+Ebling, Steven Craig
+Elliott, Jayson
+Eltringham, Kevin Eric
+Farley, James L
+Feigles, Pamela N
+Ferguson, Michael Lee
+Figueroa, Jose Anthonio
+Figueroa, Osvaldo
+Fitzwater, Todd
+Fondeur, Ricardo Hugo
+Forker, Nathan Daniel
+Foster, Ian Allen
+France, Steven
+Frederick, Adam
+Fry, Wayne A
+Funk, Joshua
+Gajus, Wesley Brandon
+Gardiner, Angela J
+Gardiner, Cadence Leiter
+Garrison, Courtney
+Gearhart III, Carl P
+Gehrer, Alexander
+Gehrer, Austin
+Gessner, Marcus
+Glica, Brendan
+Gomez, Osvaldo
+Gross, Caleb Jeffrey-Jo
+Guffey, Brian
+Haas, Bobbi Jo
+Hall, Thomas W
+Harner, Justin Sterling
+Helsley III, Stephen
+Helsley, Stephen
+Heverly, Brian K -
+Hoffman, William D
+Huhn, Christopher
+Hulsizer, Brian W
+Ikeler, William
+Irwin, Tammie S
+Jean, Shana
+Jeremiah, Tyler
+Johnson, Kelvin James
+Jones, Malcom
+Kane, Dylan Robert
+Kearney, Brian J
+Kiesinger, Sonya L
+Kiger, Steven L
+Kirby, Eric Matthew
+Kline, Justin Allen
+Klinger, Robert L.
+Knorr, Angela K
+Koch, Carson D
+Kratzer, Kean
+Kremer, Jodie
+Kurtz, Evan
+Laforme, Jordan
+Lasch, Todd
+Leon, Bradley Scott
+Leon, Brent Lee
+Leon, Devon Charlles
+Lindauer, Katelyn
+Livziey, Blaze Quinton
+Long, Allana Faye
+Long, Gregory Jonathan
+Long, Jonathon Ray
+Loss, John D
+Lucas, Christopher P
+Lutz, Brian
+Mabus, Kristen
+Martin, Chad
+Martz, Twylia-Dawn
+Maxfield, Timothy L
+McCarrick, Jason
+Miller, Keith A
+Miller, Robert D
+Miller, Tonya M
+Mingle, Charles Forrest
+Minium, Greg
+Montague, Nicholas Ryan
+Morse, Matthew
+Moyer, Curtis
+Noll, Bonnie M
+Noll, Dennis M
+Oeler, Heather M
+Ortiz, Luis Anibal
+Parker, Dustin
+Parker, Garrett
+Parrish, Shannon
+Persun, Jeremy L
+Peterson, Donovan Scott
+Porras, Jairo
+Price, Davon Marchone
+Prioleau, James Theodore
+Pulicicchio, Daren
+Ranck, McKenzie
+Reber, Brittany Ann
+Reedy, Drew T
+Reedy, Millard E
+Reigle, Kevin Eugene
+Rewolinski, Ty S
+Reynolds, Michele R
+Robbins, Colby William
+Robbins, Lonny D
+Roberts, Evelyn
+Rosado Colon, Luis A
+Ross, Chad
+Ross, Sabrena
+Rovenolt, Cathy Ann
+Sampsell, Brian Eugene
+Schreck, Kevin Dean
+Seese, Dio M
+Shipton, Blake
+Sholter, Rodney H
+Short, Charles S
+Silverman, Molly
+Simmon, Scott E
+Simon, Brandon
+Simon, Laura M
+Sims, Brian
+Smith, Cody Cheyenne
+Smith, Cody Christopher
+Smith, Kurtis Alonzo
+Snyder, Joshua James
+Snyder, Justin
+Snyder, Scott Douglas
+Staplins, Brittany Marie
+Stokes, Donnell
+Stoltzfus, Patricia C
+Strahan, David E
+Straub, Marshall Lamar -
+Strohl, Jason Wade
+Susan, Eric S
+Taggart, Ginger M
+Tallman, Zachary
+Topliff, Nicolas D
+Treibley, Kenneth M
+Try, Jeffrey
+Ulrich Jr., Timothy Leroy
+Ulrich, Robert M
+Vanover, Ronda L
+Wagner, Aaron Ray
+Wagner, Dylan Wesley
+Wagner, Walter
+Walters, Chad
+Weaver, Eric E
+Wells, Geoffrey
+Wertz, Eric Leroy
+Wesner, Preston
+West, Michael
+Whalen Jr., Frank
+White, Annette Yvonne
+Willis, Brent Ryan
+Wilson, Kyle Anthony
+Wise, Dashawn
+Wood, Joseph
+Wright, Carolyn
+Young, Thomas
+Zettlemoyer, Ronald
+`;
+employees.split("\n");
 employees.sort();
 
 let prizes = [
